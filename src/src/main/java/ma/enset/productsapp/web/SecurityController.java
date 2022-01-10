c72b9b574071baa1ac52a20246f3ee30030f1fbb
Youssef Bouchida <youssefbouchida@Youssefs-MacBook-Pro.local>
2022-01-10 18:48:15 +0100

    product-app-kc

---
+++ src/src/main/java/ma/enset/productsapp/web/SecurityController.java
@@ -0,0 +1,26 @@
+package ma.enset.productsapp.web;
+
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SecurityController {
+    @Autowired
+    private AdapterDeploymentContext adapterDeploymentContext;
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
+    }
+
+    @GetMapping("accessDenied")
+    public String error() {
+        return "accessDenied";
+    }
+}
